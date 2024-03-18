3302fc109b5124976dcb97a850eac2c6cd8ed102
Muonhoryo <muon1327@mail.ru>
2024-03-18 20:58:29 +0500

    1.7.1.0

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Text;
+
+namespace MuonhoryoLibrary
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Removes all symbols in 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="removedSymbols"></param>
+        /// <returns></returns>
+        public static string FilterBy(this string input, char[] removedSymbols)
+        {
+            if(input==null)
+                throw new ArgumentNullException("input");
+            if (removedSymbols == null)
+                throw new ArgumentNullException("removedSymbols");
+            if (input.Length==0||
+                removedSymbols.Length == 0)
+                return input;
+
+            for(int i=0; i< removedSymbols.Length; i++)
+            {
+                if (removedSymbols[i] == 0)
+                    throw new ArgumentNullException("symbol by index " + i);
+            }
+
+            bool ComparionFunc(char sym)
+            {
+                foreach (char c in removedSymbols)
+                {
+                    if (sym == c) return false;
+                }
+                return true;
+            }
+
+            StringBuilder str = new StringBuilder(input.Length);
+            foreach (var c in input)
+            {
+
+                if (ComparionFunc(c))
+                {
+                    str.Append(c);
+                }
+            }
+            return str.ToString();
+        }
+    }
+}
