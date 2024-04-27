79347888f0e799aa3286f9d0cda852068225a920
Muonhoryo <muon1327@mail.ru>
2024-04-27 11:12:23 +0500

    v1.8.0.0

---
+++ CompositeParameters/CompositeFloat.cs
@@ -0,0 +1,46 @@
+﻿using MuonhoryoLibrary.Collections;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuonhoryoLibrary
+{
+    [Serializable]
+    public sealed class CompositeFloat:CompositeParameter<float>
+    {
+        public CompositeFloat(float DefaultValue):base(DefaultValue) { }
+
+        public event Action<IConstModifier<float>> AddingAddModifierEvent;
+        public event Action<IConstModifier<float>> AddingMultiplyModifierEvent;
+
+        private readonly SingleLinkedList<ModifierHandler<float>> AddersList =
+            new SingleLinkedList<ModifierHandler<float>>() { };
+        private readonly SingleLinkedList<ModifierHandler<float>> MultipliesList = 
+            new SingleLinkedList<ModifierHandler<float>>() { };
+
+        protected override void RecalculationAction()
+        {
+            CurrentValue = DefaultValue;
+            foreach (var item in AddersList)
+            {
+                CurrentValue += item.Modifier;
+            }
+            foreach (var item in MultipliesList)
+            {
+                CurrentValue *= item.Modifier;
+            }
+        }
+
+        public IConstModifier<float> AddModifier_Add(float speed)
+        {
+            return AddModifier(this,speed, AddersList, (item) => AddingAddModifierEvent?.Invoke(item));
+        }
+        public IConstModifier<float> AddModifier_Multiply(float speed)
+        {
+            return AddModifier(this,speed, MultipliesList, (item) => AddingMultiplyModifierEvent?.Invoke(item));
+        }
+    }
+}
---
+++ CompositeParameters/CompositeParameter.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+using MuonhoryoLibrary.Collections;
+using System;
+
+namespace MuonhoryoLibrary
+{
+    /// <summary>
+    /// Default value changed by add's and multiply's modifiers
+    /// </summary>
+    public abstract class CompositeParameter<TParamType>
+    {
+        public interface IConstModifier<TParam>
+        {
+            public void RemoveModifier();
+            public TParam Modifier { get; }
+        }
+        protected sealed class ModifierHandler<TModType> : IConstModifier<TModType>
+        {
+            public ModifierHandler(TModType Modifier, SingleLinkedList<ModifierHandler<TModType>> list)
+            {
+                RemoveHandlerAction = () => list.Remove(this);
+                this.Modifier = Modifier;
+            }
+            public readonly TModType Modifier;
+            private readonly Action RemoveHandlerAction;
+            TModType IConstModifier<TModType>.Modifier => Modifier;
+            public void RemoveModifier() => RemoveHandlerAction();
+        }
+        private CompositeParameter() { }
+        public CompositeParameter(TParamType DefaultValue)
+        {
+            this.DefaultValue = DefaultValue;
+            RecalulculateValue();
+        }
+        public readonly TParamType DefaultValue;
+        public TParamType CurrentValue { get; protected set; }
+        public event Action<TParamType> ValueHasBeenRecalculatedEvent;
+        private void RecalulculateValue()
+        {
+            RecalculationAction();
+            ValueHasBeenRecalculatedEvent?.Invoke(CurrentValue);
+        }
+        protected abstract void RecalculationAction();
+
+        protected static IConstModifier<TListParamsType> AddModifier<TOwnerParamType, TListParamsType>(
+            CompositeParameter<TOwnerParamType> owner,
+            TListParamsType modifierValue,
+            SingleLinkedList<ModifierHandler<TListParamsType>> list,
+            Action<IConstModifier<TListParamsType>> runningEventAction)
+        {
+            ModifierHandler<TListParamsType> modifier =
+                new ModifierHandler<TListParamsType>(modifierValue, list);
+            list.AddLast(modifier);
+            runningEventAction(modifier);
+            owner.RecalulculateValue();
+            return modifier;
+        }
+
+        public static explicit operator TParamType(CompositeParameter<TParamType>  i) => i.CurrentValue;
+    }
+}
